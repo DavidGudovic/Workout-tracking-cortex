cae19df242775bee545bffe90f219773ed3cfeda
David <david.gudovic@datadesign.me>
2025-12-18 01:50:12 +0100

    openapi route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/docs', function () {
     return view('swagger');
 });
+
+Route::get('/openapi.json', function () {
+    return response()->file(base_path('openapi.json'));
+});
